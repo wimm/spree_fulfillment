fc32f2199994159fbf448b3805ed3341ac70e79b
wlipa <dojo@masterleep.com>
2011-07-14 21:55:43 -0700

    aha, we should be using the Shipment class

--- lib/spree_fulfillment.rb
+++ lib/spree_fulfillment.rb
@@ -12,14 +12,79 @@ module SpreeFulfillment
         Rails.env.production? ? require(c) : load(c)
       end
       
-      Order.class_eval do
+      
+      # This provides a place to remember objects that need processing outside of
+      # the restrictions on spree's order.update! method.  That is, to avoid infinite
+      # recursion.
+      
+      # Should find a cleaner way to do a per request cache, maybe something like
+      # http://stackoverflow.com/questions/660599/rails-per-request-hash
+      
+      Spree::BaseController.class_eval do
         
-        alias_method :orig_process_payments!, :process_payments!
-        def process_payments!
-          orig_process_payments!
-          Fulfillment.create_for(self)
+        before_filter :fulfill_before_filter
+        after_filter :fulfill_after_filter
+        
+        def fulfill_before_filter
+          @@fulfillment_store = [] unless defined?(@@fulfillment_store)
+        end
+
+        def fulfill_after_filter
+          Rails.logger.info '*' * 10 + "fulfill_after_filter #{@@fulfillment_store}"
+          @@fulfillment_store.each do |s|
+            # Ship everything that's hinted to be ready to ship.
+            s.ship!
+          end
+          @@fulfillment_store = []
         end
         
+        def self.fulfillment_store(x)
+          @@fulfillment_store << x
+        end
+        
+      end
+      
+      
+      Shipment.class_eval do
+        
+        
+        # Adding transition calls to the base spree_core state machine.
+        # Not following the advice in the Spree extension guide to redeclare
+        # everything, because that leads to multiple callbacks on the same edge.
+        state_machine do
+
+          # This transition does not work in the normal flow because there is logic
+          # in the shipment class inside update! to avoid the callbacks.  So we override
+          # update! as well.
+          after_transition :to => 'ready', :do => :post_ready_fulfill
+
+          before_transition :to => 'shipped', :do => :pre_ship_fulfill
+
+        end
+        
+        
+        def post_ready_fulfill
+          Rails.logger.info '*' * 10 + 'post_ready_fulfill'
+          # Remember this shipment so we can call ship on it later when it's safe to do so.
+          Spree::BaseController.fulfillment_store(self)
+        end
+        
+        def pre_ship_fulfill
+          Rails.logger.info '*' * 10 + 'pre_ship_fulfill'
+          Rails.logger.info '*' * 10 + 'YAY I AM SHIPPING'
+          self.tracking = "Y U NO SHIP"
+        end
+        
+        # The only purpose of this override is to work around spree's disabling of the
+        # state_machine callbacks during order.update!
+        alias_method :orig_update!, :update!
+        def update!(order)
+          old_state = self.state
+          orig_update!(order)
+          new_state = self.state
+          post_ready_fulfill if new_state == 'ready' and old_state != 'ready'
+        end
+      
       end
       
     end
@@ -27,5 +92,3 @@ module SpreeFulfillment
     config.to_prepare &method(:activate).to_proc
   end
 end
-
-
