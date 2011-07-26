4d03edd3b9ae115f328d0bb5122ec230ab815f97
wlipa <dojo@masterleep.com>
2011-07-26 10:57:01 -0700

    monkeypatch a spree bug with fractional sales tax pennies

--- lib/spree_fulfillment.rb
+++ lib/spree_fulfillment.rb
@@ -51,6 +51,21 @@ module SpreeFulfillment
       end
       
       
+      # Fix a bug with rounding to the penny for sales tax.
+      # See http://groups.google.com/group/spree-user/browse_thread/thread/374410d4dea30b6c
+      Calculator::SalesTax.class_eval do
+
+        alias_method :orig_compute, :compute
+        def compute(order)
+          value = orig_compute(order)
+          rounded = (value * 100).round.to_f / 100
+          Fulfillment.log "rounded #{value} to #{rounded}"
+          rounded
+        end
+        
+      end
+      
+      
       Shipment.class_eval do
         
         
